a940d1b6251e459e87d8941f0242afcbe80cae20
bernat <bvadell@feimsoft.com>
2024-10-05 03:14:33 +0200

    implement select scene world

---
+++ src/Client.Main/Worlds/SelectCharacterWorld.cs
@@ -0,0 +1,27 @@
+﻿using Client.Main.Controls;
+using Client.Main.Objects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Main.Worlds
+{
+    public class SelectCharacterWorld : WorldControl
+    {
+        public SelectCharacterWorld() : base(worldIndex: 75)
+        {
+            Camera.Instance.ViewFar = 5000f;
+            Camera.Instance.Position = new Vector3(9858, 18813, 700);
+            Camera.Instance.Target = new Vector3(7200, 19550, 550);
+        }
+
+        public override void Update(GameTime time)
+        {
+            base.Update(time);
+        }
+    }
+}
