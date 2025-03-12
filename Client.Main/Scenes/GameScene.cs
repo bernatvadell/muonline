028dc6c2d9059089e6a761f2098184c9ee38fe11
xulek <xulek121@gmail.com>
2025-03-12 19:23:34 +0100

    MapName

--- Client.Main/Scenes/GameScene.cs
+++ Client.Main/Scenes/GameScene.cs
@@ -72,6 +72,11 @@ namespace Client.Main.Scenes
 
             _main.Visible = true;
             _isChangingWorld = false;
+
+            var mapNameControl = new MapNameControl();
+            mapNameControl.LabelText = World.Name;
+            mapNameControl.BringToFront();
+            Controls.Add(mapNameControl);
         }
 
         public override void Update(GameTime gameTime)
